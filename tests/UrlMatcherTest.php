80ebdf4a5a6b20181c61a79acadf9b15920941e7
Jack <info@itlessons.info>
2014-06-15 21:36:50 +0400

    * add some tests

--- tests/UrlMatcherTest.php
+++ tests/UrlMatcherTest.php
@@ -37,6 +37,7 @@ class UrlMatcherTest extends \PHPUnit_Framework_TestCase
         $matcher->register('GET', '/search/(query:str)', 'app:search:index');
         $matcher->register('GET', '/tag/(tag:any)', 'app:tag:index');
         $matcher->register('GET', '/', 'app:home:index');
+        $matcher->register('GET', '/blog/', 'app:blog:index');
 
         $route = $matcher->match('GET', '/id777');
         $this->assertSame('app:user:index', $route->getController());
@@ -70,5 +71,12 @@ class UrlMatcherTest extends \PHPUnit_Framework_TestCase
         $route = $matcher->match('GET', '/id777/');
         $this->assertNull($route);
         $this->assertSame('/id777', $matcher->getRedirectUrl());
+
+        $route = $matcher->match('GET', '/blog');
+        $this->assertNull($route);
+        $this->assertSame('/blog/', $matcher->getRedirectUrl());
+
+        $route = $matcher->match('GET', '/blog/');
+        $this->assertSame('app:blog:index', $route->getController());
     }
 } 
